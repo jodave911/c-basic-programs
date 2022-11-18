090a339cf9c5c5b72d2cceabcd964c5c7d4d38bc
Joshua David <62954129+jodave911@users.noreply.github.com>
2022-11-18 20:58:35 +0530

    Update polymultiplication.c

--- temp/polymultiplication.c
+++ temp/polymultiplication.c
@@ -33,54 +33,43 @@ int readExpression(struct Polynomial poly[])
     return terms;
 }
 
-int multi(int firstCount, int secondCount)
-{
+int multi(int firstCount, int secondCount){
     int i, j, k=0,temp;
-    for(i=0;i<firstCount;i++)
-    {
-    	for(j=0;j<secondCount;j++)
-    	{
+    for(i=0;i<firstCount;i++){
+    	for(j=0;j<secondCount;j++){
     	    result[k].coeff = first[i].coeff * second[j].coeff;
             result[k].exp=first[i].exp + second[j].exp;
             k=k+1;
     	}
     }
+	
   display(result,k);
-    return k;
+  return k;
 }
-int compare(struct Polynomial poly[], int te)
-{
+
+int compare(struct Polynomial poly[], int te){
 	int i, j,temp,k=0,n=0;
-	for(i=0;i<te;i++)
-	{
-		for(j=i+1;j<te;j++)
-		{
-			if(result[i].exp == result[j].exp)
-			{
+	for(i=0;i<te;i++){
+		for(j=i+1;j<te;j++){
+			if(result[i].exp == result[j].exp){
 				rn[k].coeff = result[i].coeff + result[j].coeff;
             			rn[k].exp = result[i].exp;
             			result[j].coeff=0;
             			result[j].exp=0;
-  
             			k++;
             			break;
 			}
-			
 		}
-		if(j==te)
-		{
-		 
-				rn[k].coeff = result[i].coeff;
-            			rn[k].exp = result[i].exp;
-            			
-            			k++;
-            			
+		
+		if(j==te){
+			rn[k].coeff = result[i].coeff;
+            		rn[k].exp = result[i].exp;     			
+      			k++;
 		}
 	}
-	for(i=0;i<k;i++)
-	{
-		if(rn[i].coeff !=0)
-		{
+	
+	for(i=0;i<k;i++){
+		if(rn[i].coeff !=0){
 			final[n].coeff = rn[i].coeff;
             		final[n].exp = rn[i].exp;
             		n++;	
@@ -90,34 +79,23 @@ int compare(struct Polynomial poly[], int te)
 	
 }
 
+int main(){
+	int firstCount, secondCount, resultCount,new;
+	printf("\nFirst Expression:\n");
+	firstCount = readExpression(first);
+	printf("\nSecond Expression:\n");
 
+	secondCount = readExpression(second);
+	printf("\nFirst Expression");
 
-int main()
-{
-    int firstCount, secondCount, resultCount,new;
-    printf("\nFirst Expression:\n");
-    firstCount = readExpression(first);
-    printf("\nSecond Expression:\n");
-    
-    secondCount = readExpression(second);
-    printf("\nFirst Expression");
-    
-    display(first, firstCount);
-    display(second, secondCount);
-    
-    resultCount = multi(firstCount, secondCount);
-    new=compare(result,resultCount);
-    printf("\nResultant Expression:\n");
-    display(result, resultCount);
-    
-    display(final,new);
-    //printf("value of k is %d",resultCount);
-    return 0;
+	display(first, firstCount);
+	display(second, secondCount);
+
+	resultCount = multi(firstCount, secondCount);
+	new=compare(result,resultCount);
+	printf("\nResultant Expression:\n");
+	display(result, resultCount);
+
+	display(final,new);
+	return 0;
 }
-	
-	
-	
-	
-	
-	
-	
